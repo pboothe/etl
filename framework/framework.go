71eef3e27f8a5a0164ad79444b8786f7a1421406
Gregory Russell <gfr@google.com>
2017-04-02 12:27:55 -0400

    lots of package comments

--- framework/framework.go
+++ framework/framework.go
@@ -1,3 +1,36 @@
+// Package framework provides the interconnections among the components that
+// make up the m-lab pipeline.
+// Part or all of this package will likely become main.
+//
+// The components are:
+//  Driver - rpc / http interface to receive file paths. Provides Reader
+//   objects to downstream code.
+//  Reader - unpacks tar files and provides raw test data.
+//  Parsers - process individual tests
+//  BigQueryWriter - inserts new test records into BigQuery
+//  Status - handles status page generation, health reporting
+//
+// We want parallelism in the following components:
+//  Reader - because file i/o might otherwise be a bottleneck.
+//  Parser - because it is compute intensive.  We expect to want at least num_cpus
+//   parser goroutines, and expect these to consume the bulk of the CPU.
+//  BQWriter - because i/o might be a bottleneck.
+//
+// Alternative implementation:
+//  It might work equally well, or better, to have N independent goroutines, each
+//  comprising a Reader, a Parser, and a BQWriter.  We wouldn't care which
+//  of these was blocking, as long as there are enough of them, (probably 2x numcpus)
+//  so that CPUs are all kept busy.
+//
+//  One potential advantage to this is that the data would stay in the same L2 cache
+//  through it's lifetime.
+//
+//  This scheme would be quite a bit simpler from a design point of view - basically
+//  requiring only a single channel for obtaining file objects to read.
+//
+//  CONS:
+//   This would result in many large file buffers, one for each goroutine, whereas we might
+//   otherwise share a smaller number of file buffers across multiple goroutines.
 
 package framework
 
@@ -5,6 +38,17 @@ import (
 	"crypto/md5"
 	"sync"
 )
+
+type Driver interface {
+
+}
+
+// Reader is a source of tests.
+type Reader interface {
+  // Returns test as byte slice, or nil if no more tests.
+  NextTest() []byte
+}
+
 
 // replicate starts a goroutine to send 'count' copies of the
 // data block on the data channel.
